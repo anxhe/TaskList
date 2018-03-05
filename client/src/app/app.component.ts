c7f61e30a4349f0c0492ca45f7c107be683cbe4d
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2018-03-05 20:26:29 +0100

    Add configuration: - Default styles scss - Normalize - Meta viewport

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+    ]);
+  }
+
+
 }
